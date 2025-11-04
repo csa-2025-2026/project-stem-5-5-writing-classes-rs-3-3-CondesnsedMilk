cd7618d7f16ee4fb0b7f5848e47a4925c63828cc
CondesnsedMilk <achau103@student.hbuhsd.edu>
2025-11-04 22:46:44 +0000

    Problem 1

--- Main.java
+++ Main.java
@@ -4,6 +4,21 @@ public class Main
 {
   public static void main(String[] args)
   {
-    // Create objects and test your classes in here
+    Scanner scan = new Scanner(System.in);
+
+    System.out.println("Enter the person's SSN");
+    int SSN = scan.nextInt();
+
+    System.out.println("Enter the person's first name");
+    String firstName = scan.next();
+
+    System.out.println("Enter the person's last name");
+    String name = firstName + " " + scan.next();
+
+    System.out.println("Enter the person's age");
+    int age = scan.nextInt();
+
+    Person person1 = new Person(SSN, name, age);
+    person1.print();
   }
 }
--- Person.java
+++ Person.java
@@ -1,4 +1,19 @@
 public class Person
 {
+    private int SSN;
+    private String name;
+    private int age;
+
+    Person(int SSN, String name, int age)
+    {
+        this.SSN = SSN;
+        this.name = name;
+        this.age = age;
+    } 
+
+    void print()
+    {
+        System.out.printf("SSN: %d%n\tName: %s%n\tAge: %d%n", SSN, name, age);
+    }
 
 }
